bbd99abb83268c216b3f6a3de5c94fd4b48e2879
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-03 00:45:34 +0200

    mo

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def uppercase(str):
+    for i in range(len(str)):
+        if ord('a') <= ord(str[i]) <= ord('z'):
+           print("{:c}".format(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
+
+    print()
