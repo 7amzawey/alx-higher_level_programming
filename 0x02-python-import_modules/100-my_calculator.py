5bea3d06b5bf1001d450ca4621b8d9b8ef439bce
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-04 21:47:21 +0200

    error handling

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit (1)
-else:
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    c = sys.argv[2]
-    if sys.argv[2] == "+":
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        c = sys.argv[2]
+    if c == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif c == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
