fba591c518633cbad01b0d010614f37f0d06867d
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-07 17:01:11 +0200

    is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head: is the head of the list
@@ -6,4 +7,36 @@
  */
 int is_palindrome(listint_t **head)
 {
+	int len = 0, i = 0, j;
+	int *arr;
+	listint_t *copy = *head;
 
+	while (copy != NULL)
+	{
+		len++;
+		copy = copy->next;
+	}
+	arr = malloc(len * sizeof(int));
+	if (arr == NULL)
+	{
+		return (0);
+	}
+	copy = *head;
+
+	while (copy != NULL)
+	{
+		arr[i] = copy->n;
+		copy = copy->next;
+		i++;
+	}
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (arr[i] != arr[j])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+	free(arr);
+	return (1);
+}
