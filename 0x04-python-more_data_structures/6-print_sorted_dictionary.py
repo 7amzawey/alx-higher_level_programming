6930361915aa0ed67d7a72e5798dbc1c530e411f
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-10 12:19:06 +0200

    some error handling

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary)
     for key in sorted_dict:
-        print(key, end = ": ")
+        print(key, end=": ")
         print(a_dictionary[key])
