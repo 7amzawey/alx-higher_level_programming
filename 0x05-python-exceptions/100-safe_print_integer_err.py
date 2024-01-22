f216e32155f61052860640ca79a0849f44ecc4eb
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-22 11:08:09 +0200

    this day's work

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+is_int = True
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except Exception as e:
         print("Exception:", e, file=sys.stderr)
-        return False
+        is_int = False
+    return is_int          
