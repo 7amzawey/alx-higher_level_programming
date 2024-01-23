336be690d3e1a6e2132a1226c04bc9c6ae36587d
7amzawey <mohamedhamza123467890@gmail.com>
2024-01-23 12:05:57 +0200

    fine

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,10 @@
 class Square:
     """Defines a square."""
 
-
     def __init__(self, size):
         """Constructor.
 
         Args:
             size: length of a side of the square.
-            
         """
         self.__size = size
