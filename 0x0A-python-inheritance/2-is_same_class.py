7b51e1395f92ce9eda69e9964a21357d7d385072
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-15 18:56:22 +0200

    fine'

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+checking if the obj is an instance of a classs
+"""
+
+
+def is_same_class(obj, a_class):
+    """the function that will check"""
+    return type(obj) == a_class
