ba8ccf4b8475d7cff74a93049e13a7da8d79ab87
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-15 09:04:09 +0200

    some updates

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,9 +5,9 @@ Defining an empty class called Rectangle
 
 
 class Rectangle:
-    number_of_instances = 0
     """defination of the rectangle class.
     """
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """initilization of the width and height of the rectangle"""
         self.width = width
