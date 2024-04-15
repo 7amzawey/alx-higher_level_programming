e5d6a7097f5af2cff6ae88b25b050ced20a1af46
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-15 19:04:51 +0200

    fine

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define is kind of method"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if it's the same type or inhertied from the type"""
+    return (isinstance(obj, a_class))
