13e903e0f5d3aab6f33e3a9cf2a4a7ddf6ebef9a
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-16 10:13:17 +0200

    fantastic

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,9 +7,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''A subclass representing a rectangle.'''
+
     def __init__(self, width, height):
         '''constructor'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
-        self.height = height
+        self.__height = height
