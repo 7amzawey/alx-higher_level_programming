89040f13e94f1a8c3697eb10aeb072a5fa61422f
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-16 11:29:41 +0200

    fine

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,8 +2,14 @@
 """
 Defining a class MyInt"
 """
+
+
 class MyInt(int):
-    """My Int Class checks the value of an int"""
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
     def __eq__(self, other):
         if self != other:
             return not super().__eq__(other)
