a4970c9363ee7ba971b01a77bab8b8c407211ce6
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-17 08:10:27 +0200

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,11 @@ class_to_json = __import__('8-class_to_json').class_to_json
 class Student:
     """defining class Student"""
     def __init__(self, first_name, last_name, age):
+        """intiliaze the student class with three public attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """returns the dict representation of the class"""
         return (class_to_json(self))
