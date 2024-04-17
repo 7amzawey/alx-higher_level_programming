ba3ef995e433c07e6f8648bfc12114d080dec15b
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-17 08:47:46 +0200

    fine

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module is for a class student
+"""
+
+
+class Student:
+    """defining class Student"""
+    def __init__(self, first_name, last_name, age):
+        """intiliaze the student class with three public attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dict representation of the class"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dic = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dic[key] = value
+        return my_dic             
