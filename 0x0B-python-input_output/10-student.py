28ee21416a9f26665b451516c41e9bdda2fd870f
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-17 08:49:00 +0200

    10-student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -24,4 +24,4 @@ class Student:
         for key, value in self.__dict__.items():
             if key in attrs:
                 my_dic[key] = value
-        return my_dic             
+        return my_dic
