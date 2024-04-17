45e7e30971f23be506c4c162a0d8bfa350e71154
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-17 07:48:00 +0200

    json dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this module for a function that returns the dict description with simple data struct
+"""
+
+
+def class_to_json(obj):
+    """returns the dict description with simple data sturcture for a json
+    of an obj.
+    Args:
+        @obj: is the object
+    Returns: the dict of an obj
+    """
+    return obj.__dict__
