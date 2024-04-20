a5fd742c501bc29af497e3cb0c5a7a2e97615433
7amzawey <mohamedhamza123467890@gmail.com>
2024-04-20 17:02:38 +0200

    fine

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if len == 0:
+    if length == 0:
         firstchar = "None"
     else:
         firstchar = sentence[0]
