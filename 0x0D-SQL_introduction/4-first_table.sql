356d35b0bc4cc2917aca050b664e07aeab106bb0
7amzawey <mohamedhamza123467890@gmail.com>
2024-05-21 20:16:08 +0300

    create a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creates a table called first_table in the current database.
-CREATE table IF EXISTS first_table(
-	id INT
+CREATE table IF NOT EXISTS first_table(
+	id INT,
 	name VARCHAR(256)
 );
