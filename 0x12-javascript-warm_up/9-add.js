cd78dcd3e17b2480e0098abefbea152248aa03c1
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-18 18:33:42 +0200

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
-args = process.argv.slice(2);
-function add(a, b) {
-	a = args[0];
-	b = args[1];
-	console.log(a + b);
+
+function add (a, b) {
+  return a + b;
+}
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  const result = add(num1, num2);
+  console.log(result);
 }
