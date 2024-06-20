91f2b3d079d7ebfd0b76916639c83cfcbe458d64
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-20 19:18:08 +0200

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
-from 4-rectangle.js import Rectangle;
-class Square extends Rectangle {
-  constructor (size) {
-    super();
-    this.size = size;
-  }
-
+const Square = require('./5-square');
+class SquareWithCharPrint extends Square {
   charPrint (c) {
-  if (isNaN(c)) {
-    c = 'X';
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
   }
-  for (let i = 0; i <= Rectangle.height; i++) {
-    console.log(c.repeat(Rectangle.width));
-  }
-
 }
+module.exports = SquareWithCharPrint;
