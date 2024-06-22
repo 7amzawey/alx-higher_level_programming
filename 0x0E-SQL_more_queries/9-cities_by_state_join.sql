052f6284045a909b729feeb01927b063c0107987
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-22 17:07:17 +0200

    lina is moza

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- list all cities contained in the database hbtn_0d_usa
-select cities.id, cities.name, states.name as state_name from cities
-INNER JOIN states ON cities.state_id = states.id
-order by cities.id asc;
+SELECT a.id AS id, a.name AS name , b.name AS name 
+FROM cities a
+INNER JOIN states b
+on a.state_id = b.id
+ORDER BY a.id ASC ;
