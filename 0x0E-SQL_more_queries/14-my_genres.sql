ef60dc57c99e646e04c59c98f132f88b9586e655
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-23 12:53:20 +0200

    we are about to wrap up

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- my script should use the hbtn_0d_tv_shows to list all genres of the dexter
-SELECT tv_genres.name as name
-FROM tv_genres
-JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = "Dexter"
+SELECT genres.name as name
+FROM tv_genres genres
+JOIN tv_show_genres show_genres
+ON show_genres.genre_id = genres.id
+JOIN tv_shows shows
+ON shows.id = show_genres.show_id
+WHERE shows.title = "Dexter"
 ORDER BY name ASC;
