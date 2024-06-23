7fc3e4b938d67bc981dcb21c5e16543546778f2e
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-24 00:58:45 +0200

    some fixes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" This script returns all states matching a name safely from SQL injections """
+""" This script returns all
+states matching a name safely from SQL injections """
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
-    
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
@@ -11,7 +11,9 @@ if __name__ == '__main__':
                          db=argv[3])
     cur = db.cursor()
     cur.execute(
-            "SELECT c.state_id, c.name, s.name FROM states AS s JOIN cities AS c ON s.id = c.state_id ORDER BY c.id"
+            """SELECT c.state_id, c.name,
+            s.name FROM states AS s
+            JOIN cities AS c ON s.id = c.state_id ORDER BY c.id"""
             )
     rows = cur.fetchall()
     for row in rows:
