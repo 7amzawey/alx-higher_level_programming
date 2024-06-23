88d30d6b03607a2eff5debb25f173120a5fb8419
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-23 13:26:48 +0200

    mando is done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- my script should use the hbtn_0d_tv_shows to list all genres of the dexter
-SELECT shows.title AS title
-tv_genres.name as name
+SELECT shows.title AS title,
+genres.name AS name
 FROM tv_shows shows
-JOIN tv_show_genres show_genres 
+LEFT JOIN tv_show_genres show_genres 
 ON shows.id = show_genres.show_id
-JOIN tv_genres genres
+LEFT JOIN tv_genres genres
 ON show_genres.genre_id = genres.id
 ORDER BY title, name ASC;
