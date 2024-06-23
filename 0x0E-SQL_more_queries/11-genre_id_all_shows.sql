f9837a40cb6cc490a576a4a06054ec813e88c891
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-23 03:41:40 +0200

    hard work pays

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- this script lists all the shows contained in hbtn_0d_tvshows
+SELECT shows.title, genres.genre_id
+FROM tv_shows shows
+LEFT JOIN tv_show_genres genres
+ON shows.id = genres.show_id
+ORDER BY shows.title, genres.genre_id;
