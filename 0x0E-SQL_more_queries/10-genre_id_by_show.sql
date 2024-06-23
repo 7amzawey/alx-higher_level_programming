197e533301e02377679b8fb07ca590765b175cc7
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-23 03:37:41 +0200

    doing some fixes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- this script lists all the shows contained in hbtn_0d_tvshows
-select tv_shows.title, tv_show_genres.genre_id
-from tv_shows
-join tv_show_genres
-on tv_shows.id = tv_show_genres.show_id
-order by tv_shows.title, tv_show_genres.genre_id;
+select shows.title, genres.genre_id
+from tv_shows shows
+join tv_show_genres genres
+on shows.id = genres.show_id
+order by shows.title, genres.genre_id;
