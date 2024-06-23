2757dedd0972e808d000d4c7cba4f31a12d624d6
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-24 00:21:35 +0200

    fine'g

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" This script return all states """
+""" This script returns all states matching a name safely from SQL injections """
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
+    
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".
-            format(argv[4]))
+    # Use parameterized query to prevent SQL injection
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
