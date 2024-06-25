907da2f90dd948fdaeaf30b92c2c2ef76fe234f1
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-25 13:14:29 +0200

    delet baby delet

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+this module is for a function that prints the first object of a database
+"""
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
+            pool_pre_ping=True
+            )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+this module is for a function that prints the first object of a database
+"""
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
+            pool_pre_ping=True
+            )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(
+            State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    for state in states:
+        session.delete(state)
+    session.commit()
+
+session.close()
