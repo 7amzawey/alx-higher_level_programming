3921e56cdaa8ba3643f9a2a590b0ecc974e8ec03
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-28 19:43:57 +0200

    let's see

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
-"""
-this is gonna create the state California with the city San Francisco
+"""101-relationship_states_cities_list module
+lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
 """
 
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
-from relationship_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+from relationship_state import Base, State
 from relationship_city import City
 
-
-if __name__ == "__main__":
-    import sys
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+if __name__ == '__main__':
     engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
-            pool_pre_ping=True
-            )
-
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_city = City(name="mo", state_id=100)
-
-    states = session.query(
-            State).options(
-                    joinedload(State.cities)).all()
-
-    states_sorted = sorted(
-            states, key=lambda s: (s.id, ([c.id for c in s.cities])))
-    for state in states_sorted:
-        print(f"{state.id}: {state.name}")
+    states = session.query(State).all()
+    for state in states:
+        print(f'{state.id}: {state.name}')
         for city in state.cities:
-            print(f"    {city.id}: {city.name}")
-    session.close()
+            print(f'    {city.id}: {city.name}')
