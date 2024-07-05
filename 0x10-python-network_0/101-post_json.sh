0af961be8a7ca877019f620267204b42f97dcbb6
root <7amzawey>
2024-07-05 04:55:54 -0700

    let's see

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a json post request to a url passed as the first argument
-# and displays the body of the response
 curl -s -X Post -H "Content-Type: application/json" -d "$2" "$1"
