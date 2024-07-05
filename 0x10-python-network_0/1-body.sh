068c7dfae8c5f2cd92447437d41e63f0fe88fd69
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 09:57:26 +0200

    some changes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# thais script takes in a URL and sends a request to that URL and display size
-response=$(curl -s -w "%{http_code}" "$1")
-body=$(echo "$response" | sed '$d')
-status_code=$(echo "$response" | tail -n1)
-
-if [ "$status_code" -eq 200 ]; then
-	  echo "$body"
-fi
+# thais script takes in a URL and sends a request to that URL and display body
+curl -s -o /dev/null -w "%{http_code}" "$1"
