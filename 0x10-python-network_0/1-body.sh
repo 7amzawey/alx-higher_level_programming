35f536e8c4fa7a94a03e6fb2e0cd73cea5569f7d
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 10:10:44 +0200

    let's see

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # thais script takes in a URL and sends a request to that URL and display body
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | { read http_status; if [ "$http_status" -eq 200 ]; then curl -sL "$1"; fi; }
