b48fa695ea1f8676bd8f9c8f5b4b3a66dcc699cf
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 20:53:21 +0200

    nice

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """this module is for fectching a  specific url"""
 
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
 
-import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    resp = response.read()
-    decoded_resp = resp.decode('utf-8')
-    print("Body response:")
-    print(f"    - type: {type(resp)}")
-    print(f"    - content: {resp}")
-    print(f"    - utf8 content: {decoded_resp}")
+    with urllib.request.urlopen(url) as response:
+        resp = response.read()
+        decoded_resp = resp.decode('utf-8')
+        print("Body response:")
+        print(f"    - type: {type(resp)}")
+        print(f"    - content: {resp}")
+        print(f"    - utf8 content: {decoded_resp}")
