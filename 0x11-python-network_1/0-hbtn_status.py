5ea24a0dee7c8c100a906fb7108ac6a0bc87210b
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 11:31:26 -0700

    mo rocks programming sucks

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""this module is for fectching a  specific url"""
+
+
+import urllib.request
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    resp = response.read()
+    decoded_resp = resp.decode('utf-8')
+    print(f"Body response:")
+    print(f"    -type: {type(resp)}")
+    print(f"    -content: {resp}")
+    print(f"    -utf8 content: {decoded_resp}")
