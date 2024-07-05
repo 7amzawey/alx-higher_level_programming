81ae05788da4bfa51c9fa5010fb68d5dc31cfd9a
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 22:40:47 +0200

    fine

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,11 +3,10 @@
 
 
 if __name__ == '__main__':
-    import requests   
+    import requests
     import sys
 
     url = sys.argv[1]
     email = sys.argv[2]
-
     response = requests.post(url, data={email: 'email'})
-    print(reponse.text)
+    print(response.text)
