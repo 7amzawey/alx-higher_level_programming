268bbd8cd59fdc4b59683e89a296307100af33d5
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 22:45:24 +0200

    it should be right now

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     email = sys.argv[2]
-    response = requests.post(url, data={email: 'email'})
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
     print(response.text)
