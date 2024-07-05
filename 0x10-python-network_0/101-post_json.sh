0199b5dacd12e78f5412d5463ddd65b58290c7d5
root <7amzawey>
2024-07-05 05:00:06 -0700

    mo rocks programming ducks

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request to a url passed as the first argument
-curl -s -X Post -H "Content-Type: application/json" -d "$2" "$1"
+curl -s -X Post -H "Content-Type: application/json" -d @"$2" "$1"
