f2de31fcb29e7aab645604df8eb6eb7749ab7b00
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 22:58:05 +0200

    nice

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     url = sys.argv[1]
     try:
         response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+
         print(response.text)
 
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
+    except requests.exceptions.HTTPError as e:  # Corrected to `requests.exceptions.HTTPError`
+        print(f"Error code: {e.response.status_code}")  # Corrected `e.response.stauts_code` to `e.response.status_code`
