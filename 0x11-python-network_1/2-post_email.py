b079bc587bdb92df388141c91a0a6ae62f4ac08e
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-05 21:39:43 +0200

    some changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     request = urllib.request.Request(url, data=encoded_data, method='POST')
 
     with urllib.request.urlopen(request) as response:
-        print(response.read().decode('utf-8'))    
+        print(response.read().decode('utf-8'))
