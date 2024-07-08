54ae0c646f2b8fb2435debc5482781a1164f9359
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-08 13:53:11 +0200

    nice

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,12 +11,9 @@ if __name__ == '__main__':
 
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    
     params = {'per_page': 10}
     url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
-    
     response = requests.get(url, params=params)
-    
     commits = response.json()
     for commit in commits:
         sha = commit['sha']
