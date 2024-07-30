18edd2bde946331b96770f4da5e98f998156eed3
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-30 15:41:59 +0200

    quick fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.writeFile(filePath, 'utf-8', (error, data) => {
+fs.readFile(filePath, 'utf-8', (error, data) => {
   if (error) {
     console.log(error);
+  } else {
+    console.log(data);
   }
 });
