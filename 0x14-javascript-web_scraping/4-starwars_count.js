823a25121f4d2de7dd8620d7d5b0c6cc48cd7af8
7amzawey <mohamedhamza123467890@gmail.com>
2024-07-30 17:49:56 +0200

    nothing stops in the face of the great pharoah of Egypt

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+let i = 0; let j = 0; let count = 0;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(`${error.message}`);
+    console.log(response);
+  }
+  const jsonResp = JSON.parse(body);
+  if (jsonResp.results[0].characters[0] !== 'https://swapi-api.alx-tools.com/api/people/1/') {
+    console.log('nice');
+  }
+  while (jsonResp.results[i]) {
+    while (jsonResp.results[i].characters[j]) {
+      if (jsonResp.results[i].characters[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count++;
+      }
+      j++;
+    }
+    j = 0;
+    i++;
+  }
+  console.log(count);
+});
